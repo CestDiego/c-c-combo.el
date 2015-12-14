72d390fd83e72917f8c79532de3eeeed13c80788
Diego Berrocal <cestdiego@gmail.com>
2015-12-14 18:46:45 -0500

    Refactor code
    Change last-key to alist that consists of keys: [timestamp of last key,
    last key's value, if the key pressed is repeated]
    
    Upped a little bit the Words per Minute

--- C-c-combo.el
+++ C-c-combo.el
@@ -18,18 +18,36 @@
 ;; This mode is super awesome and you should feel the same about it or else..
 ;;
 
-(defvar C-c-combo--last-time  0
-  "Last Keystroke TimeStamp")
+(defvar C-c-combo--last-key  '(("timestamp" . 0)
+                               ("key" . nil)
+                               ("repeated?". nil))
+  "Cons Cell, first item is timestamp second is the key")
 (defvar C-c-combo--curr-cps  0
   "Moving Average Rate in Characters per second")
-(defvar C-c-combo--target-wpm 50
+(defvar C-c-combo--target-wpm 60.0
   "Words per Minute Target Rate")
-(defvar C-c-combo--target-cps 4.1666
+(defvar C-c-combo--target-cps 5.83
   "Characters per second Target Rate")
 (defvar C-c-combo-check-timer nil
   "Timer that checks if we are over the target CPS")
 (defvar C-c-combo--counter 0
   "Stores how many seconds you have been with acceptable wpm")
+
+;; (defconst C-c-combo--files-path (file-name-directory load-file-name))
+(setq C-c-combo--files-path "/home/jarvis/Projects/C-c-combo/")
+(setq C-c-combo--announcer-files-path '())
+
+(defun get-announcer-file-paths ()
+  (unless (= 4 (length C-c-combo--announcer-files-path))
+    (setq C-c-combo--announcer-files-path
+          (mapcar (lambda (file)
+                    (expand-file-name
+                     (concat file ".wav")
+                     C-c-combo--files-path))
+                  '("fatality"
+                    "flawless_spree"
+                    "unstoppable"
+                    "stop_this_modafoca")))))
 
 (defun C-c-combo-set-target-rate (rate)
   (setq C-c-combo--target-wpm rate
@@ -44,25 +62,38 @@
          (chars-per-sec (/ chars-per-min 60.0)))
     chars-per-sec))
 
-(defun make-sound ()
-  (message "sth"))
+;;;###autoload
+(defun C-c-combo--play-sound-file (path)
+  (if (eq system-type 'darwin)
+      (start-process "*Messages*" nil "afplay" path)
+    (start-process "*Messages*" nil "aplay" path)))
 
-(defun do-it ()
-  (when (equal (mod C-c-combo--counter 3) 0)
-    (make-sound))
+(defun C-c-combo--play-announcer-sound ()
+  "This will end when our list ends."
+  (let ((current-sound (pop C-c-combo--announcer-files-path)))
+    (when current-sound
+      (C-c-combo--play-sound-file current-sound))))
+
+(defun C-c-combo--encourage-user ()
+  (when (equal (mod C-c-combo--counter 5) 0)
+    (C-c-combo--play-announcer-sound))
   (setq C-c-combo--counter (1+ C-c-combo--counter)))
 
 (defun C-c-combo--check-if-over-target-rate ()
-  (setq C-c-combo--curr-cps (C-c-combo--compute-cps))
-  (if (> C-c-combo--curr-cps C-c-combo--target-cps)
-      (do-it)
-    (setq C-c-combo--counter 0)))
+  (let ((repeated? (assoc-default "repeated?" C-c-combo--last-key))
+        (computed-cps (C-c-combo--compute-cps)))
+    (setq C-c-combo--curr-cps computed-cps)
+    (if (and (not repeated?) (> computed-cps C-c-combo--target-cps))
+        (C-c-combo--encourage-user)
+      (get-announcer-file-paths)
+      (setq C-c-combo--counter 0))))
 
 (defun C-c-combo--compute-cps ()
-  (let* ((now      (C-c-combo--current-time-in-seconds))
-         (interval (- now C-c-combo--last-time))
-         (exponent (* interval C-c-combo--target-cps))
-         (base     (- 1.0 (/ 1.0 C-c-combo--target-cps)))
+  (let* ((now        (C-c-combo--current-time-in-seconds))
+         (last-time  (assoc-default "timestamp" C-c-combo--last-key))
+         (interval   (- now last-time))
+         (exponent   (* interval C-c-combo--target-cps))
+         (base       (- 1.0 (/ 1.0 C-c-combo--target-cps)))
          (decay-factor (expt base exponent))
          (new-rate (* C-c-combo--curr-cps decay-factor)))
     new-rate))
@@ -72,10 +103,13 @@
     (when (and (this-command-keys)
                (derived-mode-p 'prog-mode))
       (let ((now      (C-c-combo--current-time-in-seconds))
+            (key      (this-command-keys))
+            (last-key (assoc-default "key" C-c-combo--last-key))
             (new-rate (+ 1 (C-c-combo--compute-cps))))
-        ;; weird
         (setq C-c-combo--curr-cps new-rate
-              C-c-combo--last-time now)))))
+              C-c-combo--last-key `(("timestamp" . ,now)
+                                    ("key"       . ,key)
+                                    ("repeated?" . ,(equal key last-key))))))))
 
 ;;;###autoload
 (defun C-c-combo--activate ()
@@ -104,7 +138,7 @@
 (define-minor-mode C-c-combo-mode
   "C-c-combo"
   :global t
-  :lighter ""
+  :lighter nil
   :keymap nil
   :init-value nil
   (C-c-combo--toggle))
