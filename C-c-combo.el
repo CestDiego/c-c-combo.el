c51b2c80ff34d0de12d24e758a1b16edf0dc48cb
Diego Berrocal <cestdiego@gmail.com>
2015-12-13 23:10:44 -0500

    Add Licence

--- C-c-combo.el
+++ C-c-combo.el
@@ -1,3 +1,23 @@
+;; Copyright (C) 2015  Diego Berrocal
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Commentary:
+;;
+;; This mode is super awesome and you should feel the same about it or else..
+;;
+
 (defvar C-c-combo--last-time  0
   "Last Keystroke TimeStamp")
 (defvar C-c-combo--curr-cps  0
